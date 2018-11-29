07c0d3624ca89084f90b5fc5dc82dd3c0f579eef
Kristófer Már Gíslason <kmg14@hi.is>
2018-11-29 22:38:58 +0000

    Röðun komin

--- src/index.js
+++ src/index.js
@@ -80,15 +80,18 @@ document.addEventListener('DOMContentLoaded', () => {
       success: function(data) {
         $(data.lectures).each(function(index,value) {
 
-          $(".lecture__category--"+index).append("<p> "+  value.category + "</p>");
+          $(".lecture__category--"+count).append("<p> "+  value.category + "</p>");
           $(".lecture__title--"+count).append("<h1>"+  value.title + "</h1>");
           if(value.thumbnail !== undefined) {$(".lecture__img--"+count).append("<img src='"+ "../" + value.thumbnail + "' ></img>");}
           $("#sectionLecture"+count).wrap("<a class = 'link' href='"+ "../fyrirlestur.html?slug=" + value.slug + "'></a>");
 
           count++;
-
      });
 
+
+     /*
+      * Geymir element sem hægt er að bæta við eftir að búið er að eyða
+      */
      cacheDOM_1 = $("#col1"); cacheDOM_2 = $("#col2");
      cacheDOM_3 = $("#col3"); cacheDOM_4 = $("#col4");
      cacheDOM_5 = $("#col5"); cacheDOM_6 = $("#col6");
@@ -96,140 +99,171 @@ document.addEventListener('DOMContentLoaded', () => {
      cacheDOM_9 = $("#col9"); cacheDOM_10 = $("#col10");
      cacheDOM_11 = $("#col11"); cacheDOM_12 = $("#col12");
      cacheDOM_13 = $("#col13");
+     const cacheDom = [cacheDOM_1,cacheDOM_2,cacheDOM_3,cacheDOM_4,cacheDOM_5,
+                      cacheDOM_6,cacheDOM_7,cacheDOM_8,cacheDOM_9,cacheDOM_10,
+                      cacheDOM_11,cacheDOM_12,cacheDOM_13];
 
+
+      // Hjálparföll til þess að bætta við elementum
+      function addSome(y){
+        for(let x = 0; x<y; x++)
+          $(".lectures__row").append(cacheDom[x]);
+      }
+
+      function addSomeSpace(x,y){
+        x = x -1;
+        y = y -1;
+          while (x<=y){
+            $(".lectures__row").append(cacheDom[x]);
+            x++;
+        }
+      }
+
+     function addAll() {
+       $(".lectures__row").append(cacheDOM_1);
+       $(".lectures__row").append(cacheDOM_2);
+       $(".lectures__row").append(cacheDOM_3);
+       $(".lectures__row").append(cacheDOM_4);
+       $(".lectures__row").append(cacheDOM_5);
+       $(".lectures__row").append(cacheDOM_6);
+       $(".lectures__row").append(cacheDOM_7);
+       $(".lectures__row").append(cacheDOM_8);
+       $(".lectures__row").append(cacheDOM_9);
+       $(".lectures__row").append(cacheDOM_10);
+       $(".lectures__row").append(cacheDOM_11);
+       $(".lectures__row").append(cacheDOM_12);
+       $(".lectures__row").append(cacheDOM_13);
+     }
+
+     function removeAll() {
+       for(let x = 1; x<14; x++)
+           $("#col"+x).remove();
+     }
 
      /**  EventListener fyrir takka á index.html
        *  litar takka þegar smellt er á þá og raðar lectures
        **/
 
-     css_btn.addEventListener("click", function(){
+        js_btn.addEventListener("click", function(){
 
-       for(let x = 1; x<14; x++)
-           $("#col"+x).remove();
+          // Eyða öllum elementum
+          removeAll();
+          // Bæta við fyrstu 8-13 elementum
+          addSomeSpace(8,13);
 
-           $(".lectures__row").append(cacheDOM_4);
-           $(".lectures__row").append(cacheDOM_5);
-           $(".lectures__row").append(cacheDOM_6);
-           $(".lectures__row").append(cacheDOM_7);
 
-       if(!css_clicked) {
-         css_btn.style.backgroundColor = "#2d2";
-         css_clicked = true;
+          if(!js_clicked){
+            js_btn.style.backgroundColor = "#2d2";
+            js_clicked = true;
 
-         if (html_clicked){
-           $(".lectures__row").append(cacheDOM_1);
-           $(".lectures__row").append(cacheDOM_2);
-           $(".lectures__row").append(cacheDOM_3);
+
+            if (css_clicked)
+              addSomeSpace(4,7);
+
+            if (html_clicked)
+              addSome(3);
+
           }
+          else {
 
-          if (js_clicked){
-            $(".lectures__row").append(cacheDOM_8);
-            $(".lectures__row").append(cacheDOM_9);
-            $(".lectures__row").append(cacheDOM_10);
-            $(".lectures__row").append(cacheDOM_11);
-            $(".lectures__row").append(cacheDOM_12);
-            $(".lectures__row").append(cacheDOM_13);
-           }
-         }
-       else {
-         for(let x = 1; x<14; x++)
-             $("#col"+x).remove();
+            for (let i = 8 ; i<=13 ; i++)
+                $("#col"+i).remove();
 
-         $(".lectures__row").append(cacheDOM_3);
-         $(".lectures__row").append(cacheDOM_4);
-         $(".lectures__row").append(cacheDOM_5);
-         $(".lectures__row").append(cacheDOM_6);
-         css_btn.style.backgroundColor = "#ccc";
-         css_clicked = false;
-     }});
+            if (css_clicked)
+              addSomeSpace(4,7);
 
-      js_btn.addEventListener("click", function(){
+            if (html_clicked)
+              addSome(3);
 
-        for(let x = 1; x<14; x++)
-            $("#col"+x).remove();
+            if ( !html_clicked && !css_clicked) {
+                removeAll();
+                addAll();
+              }
 
-            $(".lectures__row").append(cacheDOM_8);
-            $(".lectures__row").append(cacheDOM_9);
-            $(".lectures__row").append(cacheDOM_10);
-            $(".lectures__row").append(cacheDOM_11);
-            $(".lectures__row").append(cacheDOM_12);
-            $(".lectures__row").append(cacheDOM_13);
-
-        if(!js_clicked) {
-          js_btn.style.backgroundColor = "#2d2";
-          js_clicked = true;
-
-          if (html_clicked){
-            $(".lectures__row").append(cacheDOM_1);
-            $(".lectures__row").append(cacheDOM_2);
-            $(".lectures__row").append(cacheDOM_3);
-           }
-
-          if (css_clicked){
-            $(".lectures__row").append(cacheDOM_4);
-            $(".lectures__row").append(cacheDOM_5);
-            $(".lectures__row").append(cacheDOM_6);
-            $(".lectures__row").append(cacheDOM_7);
-            }
+            js_btn.style.backgroundColor = "#ddd";
+            js_clicked = false;
           }
-        else {
-          for(let x = 1; x<14; x++)
-              $("#col"+x).remove();
-          $(".lectures__row").append(cacheDOM_1);
-          $(".lectures__row").append(cacheDOM_2);
-          $(".lectures__row").append(cacheDOM_3);
-          $(".lectures__row").append(cacheDOM_4);
-          $(".lectures__row").append(cacheDOM_5);
-          $(".lectures__row").append(cacheDOM_6);
-          $(".lectures__row").append(cacheDOM_7);
-          js_btn.style.backgroundColor = "#ccc";
-          js_clicked = false;
-      }});
+        });
+
+
+
+        css_btn.addEventListener("click", function(){
+          // Eyða öllum elementum
+          removeAll();
+          // Bæta við fyrstu 4-7 elementum
+          addSomeSpace(4,7);
+
+          if(!css_clicked){
+            css_btn.style.backgroundColor = "#2d2";
+            css_clicked = true;
+
+            if (html_clicked)
+              addSome(3);
+
+            if (js_clicked)
+              addSomeSpace(8,13);
+
+          }
+          else {
+
+            css_btn.style.backgroundColor = "#ddd";
+            css_clicked = false;
+
+            for (let i = 4 ; i<=7 ; i++)
+                $("#col"+i).remove();
+
+            if (html_clicked)
+              addSome(3);
+
+            if (js_clicked)
+              addSomeSpace(8,13);
+
+            if ( !js_clicked && !css_clicked) {
+                removeAll();
+                addAll();
+              }
+
+          }
+        });
+
+
 
       html_btn.addEventListener("click", function(){
 
-        for(let x = 1; x<14; x++)
-            $("#col"+x).remove();
-
-            $(".lectures__row").append(cacheDOM_1);
-            $(".lectures__row").append(cacheDOM_2);
-            $(".lectures__row").append(cacheDOM_3);
+        // Eyða öllum elementum
+        removeAll();
+        // Bæta við fyrstu 3 elementum
+        addSome(3);
 
         if(!html_clicked) {
           html_btn.style.backgroundColor = "#2d2";
           html_clicked = true;
 
-          if (css_clicked){
-            $(".lectures__row").append(cacheDOM_4);
-            $(".lectures__row").append(cacheDOM_5);
-            $(".lectures__row").append(cacheDOM_6);
-            $(".lectures__row").append(cacheDOM_7);
-          }
+          if (css_clicked)
+            addSomeSpace(4,7);
 
-          if (js_clicked){
-            $(".lectures__row").append(cacheDOM_8);
-            $(".lectures__row").append(cacheDOM_9);
-            $(".lectures__row").append(cacheDOM_10);
-            $(".lectures__row").append(cacheDOM_11);
-            $(".lectures__row").append(cacheDOM_12);
-            $(".lectures__row").append(cacheDOM_13);
-           }
-        }
+          if (js_clicked)
+            addSomeSpace(8,13);
+      }
+
         else  {
-          for(let x = 1; x<14; x++)
-              $("#col"+x).remove();
-          $(".lectures__row").append(cacheDOM_4);
-          $(".lectures__row").append(cacheDOM_5);
-          $(".lectures__row").append(cacheDOM_6);
-          $(".lectures__row").append(cacheDOM_7);
-          $(".lectures__row").append(cacheDOM_8);
-          $(".lectures__row").append(cacheDOM_9);
-          $(".lectures__row").append(cacheDOM_10);
-          $(".lectures__row").append(cacheDOM_11);
-          $(".lectures__row").append(cacheDOM_12);
-          $(".lectures__row").append(cacheDOM_13);
-          html_btn.style.backgroundColor = "#ccc";
-          html_clicked = false;
+
+          for (let i = 1 ; i<4 ; i++)
+              $("#col"+i).remove();
+
+          if (css_clicked)
+            addSomeSpace(4,7);
+
+          if (js_clicked)
+            addSomeSpace(8,13);
+
+          if ( !js_clicked && !css_clicked) {
+              removeAll();
+              addAll();
+            }
+
+        html_btn.style.backgroundColor = "#ccc";
+        html_clicked = false;
 
       }});
    }
